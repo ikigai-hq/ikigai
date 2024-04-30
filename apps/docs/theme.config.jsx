44ef905cf3a4bf31e8b60ab53f857f2650fc10f1
cptrodgers <cptrodgers@gmail.com>
2024-04-30 11:13:29 +0700

    Init Documents Page

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+    logo: (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+                src="https://raw.githubusercontent.com/ikigai-hq/ikigai/master/assets/logo/logo.png"
+                alt="ikigai"
+                width="50px" />
+            <p style={{ fontSize: "24px", fontWeight: "700" }}>Ikigai</p>
+        </div>
+    ),
+    project: {
+        link: 'https://github.com/ikigai-hq/ikigai',
+    }
+}

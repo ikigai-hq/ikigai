055b6fd28578fe7edff41a97a983195ecb813aec
cptrodgers <cptrodgers@gmail.com>
2024-04-30 14:32:15 +0700

    [Document]: Add community and discord

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,35 +1,44 @@
 export default {
-    logo: (
-        <div style={{ display: "flex", alignItems: "center" }}>
-            <img
-                src="https://raw.githubusercontent.com/ikigai-hq/ikigai/master/assets/logo/logo.png"
-                alt="ikigai"
-                width="50px" />
-            <p style={{ fontSize: "24px", fontWeight: "700" }}>Ikigai</p>
-        </div>
-    ),
-    project: {
-        link: 'https://github.com/ikigai-hq/ikigai',
-    },
-    docsRepositoryBase: "https://github.com/ikigai-hq/ikigai/tree/master/apps/docs",
-    useNextSeoProps() {
-        return {
-            titleTemplate: '%s – ikigai - Open Assignment Platform'
-        }
-    },
-    head: (
-        <>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta property="og:title" content="Ikigai" />
-            <meta property="og:description" content="Open Assignment Platform" />
-        </>
-    ),
-    banner: {
-        key: '2.0-release',
-        text: (
-            <a href="https:https://github.com/ikigai-hq/ikigai/releases/tag/v0.1" target="_blank">
-                🎉 Ikigai 0.1 is released. Read more →
-            </a>
-        )
-    },
+  logo: (
+    <>
+      <img
+        src="https://raw.githubusercontent.com/ikigai-hq/ikigai/master/assets/logo/logo.svg"
+        alt="ikigai"
+        width="24px" />
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>Ikigai</span>
+    </>
+  ),
+  chat: {
+    link: 'https://discord.gg/XuYWkn6kUS',
+  },
+  project: {
+    link: 'https://github.com/ikigai-hq/ikigai',
+  },
+  docsRepositoryBase: "https://github.com/ikigai-hq/ikigai/tree/master/apps/docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ikigai - Open Assignment Platform'
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Ikigai" />
+      <meta property="og:description" content="Open Assignment Platform" />
+      <link rel="icon" href="https://raw.githubusercontent.com/ikigai-hq/ikigai/master/assets/logo/logo.svg" />
+    </>
+  ),
+  banner: {
+    key: '2.0-release',
+    text: (
+      <a href="https:https://github.com/ikigai-hq/ikigai/releases/tag/v0.1" target="_blank">
+        🎉 Ikigai 0.1 is released. Read more →
+      </a>
+    )
+  },
+  footer: {
+    text: (
+      <span>2024 ©{' '} <a href="https://ikigai.li" target="_blank">Ikigai</a></span>
+    )
+  },
 }

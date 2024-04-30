6b85730d03a820d379641b3a39532bc8fed2cc5b
cptrodgers <cptrodgers@gmail.com>
2024-04-30 13:41:03 +0700

    chore: custom SEO for ikigai page

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -12,5 +12,24 @@ export default {
         link: 'https://github.com/ikigai-hq/ikigai',
     },
     docsRepositoryBase: "https://github.com/ikigai-hq/ikigai/tree/master/apps/docs",
-    footer: "Copyright @ ikigai"
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – ikigai - Open Assignment Platform'
+        }
+    },
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content="Ikigai" />
+            <meta property="og:description" content="Open Assignment Platform" />
+        </>
+    ),
+    banner: {
+        key: '2.0-release',
+        text: (
+            <a href="https:https://github.com/ikigai-hq/ikigai/releases/tag/v0.1" target="_blank">
+                🎉 Ikigai 0.1 is released. Read more →
+            </a>
+        )
+    },
 }
